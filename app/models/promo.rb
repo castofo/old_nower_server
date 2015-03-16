b0e1bfa2ac88d85360a32812d3822a7ff2d7c5f4
Stiven Lopera <jlopera8@eafit.edu.co>
2015-03-16 17:39:29 -0500

    Trying to generate a conflict

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -4,6 +4,6 @@ class Promo < ActiveRecord::Base
   validates :terms, presence: true
   validates :expiration_date, presence: true
   validates :people_limit, presence: true
-  
+  # Hola prueba
   has_and_belongs_to_many :branches
 end
