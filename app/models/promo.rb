7898d4298b4e9a55120bc9a0c8ca3cd799c1308d
EstebanFS <seforonda@gmail.com>
2015-03-16 11:11:54 -0500

    This is a comment

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -4,6 +4,6 @@ class Promo < ActiveRecord::Base
   validates :terms, presence: true
   validates :expiration_date, presence: true
   validates :people_limit, presence: true
-  
+  #Hola esto es un comentario
   has_and_belongs_to_many :branches
 end
