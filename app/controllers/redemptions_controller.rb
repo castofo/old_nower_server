2b055ecd3d22383377e7d702eb829c8b0d0a29f5
Santiago Vanegas Gil <savanegasg@gmail.com>
2015-04-19 14:39:27 -0500

    Promo cannot be taken if it is expired (in time or people limit). Modifying errors structure in take promo service

--- app/controllers/redemptions_controller.rb
+++ app/controllers/redemptions_controller.rb
@@ -5,23 +5,23 @@ class RedemptionsController < ApplicationController
     user_id = generate_code_params["user_id"]
     promo = Promo.find_by id: promo_id
     user = User.find_by id: user_id
-    if !promo
-      render json: {
-        success: false,
-        errors: ["Invalid promo"]
-      }
-    elsif !user
-      render json: {
-        success: false,
-        errors: ["Invalid user"]
-      }
-    elsif Redemption.find_by user: user, promo: promo
-      render json: {
-        success: false,
-        errors: ["You already picked this promo"]
-      }
-    else
-      redemption = Redemption.new
+    redemption = Redemption.new
+
+    # Catching errors
+    redemption.errors.add(:promo, "is invalid") if !promo
+    redemption.errors.add(:user, "is ivalid") if !user
+    if user && promo && Redemption.find_by(user: user, promo: promo)
+      redemption.errors.add(:promo, "was already taken by you")
+    end
+    if promo && promo.has_expired
+      redemption.errors.add(:promo, "has already expired")
+    end
+    if promo && promo.available_redemptions == 0
+      redemption.errors.add(:promo, "has no more stocks")
+    end
+
+    if !redemption.errors.any?
+      # No errors, continue to create the redemption
       redemption.user = user
       redemption.promo = promo
       begin
@@ -33,13 +33,13 @@ class RedemptionsController < ApplicationController
           redemption: redemption
         },
         only: [:success, :redemption, :code, :promo_id, :user_id, :redeemed]
-      else
-        render json: {
-          success: false,
-          errors: redemption.errors
-        }
+        return # KEEP THIS or a double render will occur
       end
     end
+    render json: {
+      success: false,
+      errors: redemption.errors
+    }
   end
 
   def redeem
