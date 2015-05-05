8fb593bc34c40e61d143a41e0d781b8d19642065
Santiago Vanegas Gil <savanegasg@gmail.com>
2015-05-05 13:26:38 -0500

    Spanish support for default rails messages in all models, now, spanish is default language

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,8 +1,8 @@
 class Store < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true,
-  format: {
-    with: /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}/
-  }
+                    format: {
+                      with: /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}/
+                    }
   validates :name, presence: true
   validates :main_phone, presence: true
   validates :password, presence: true, confirmation: true
